f317977cab45e2c0a9eb095faf088ec665f9116f
Clément Wds <71884576+Clement-Wds@users.noreply.github.com>
2024-02-01 12:26:51 +0100

    config sequelize

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mysql from 'mysql2';
+import sequelize from './src/sequelize.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+// Middleware d'erreur global
+app.use((err, req, res, next) => {
+  res.status(404).send('Demande non trouvée');
+  res.status(500).send('Erreur : ' + err.message);
+});
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -12,6 +20,38 @@ if (!jwtSecret) {
   console.error("La clé secrète JWT n'est pas définie dans le fichier .env");
   process.exit(1);
 }
+
+// Middleware Body Parser
+app.use(express.json());
+
+// Configuration de la base de données
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// Connexion à la base de données
+db.connect(err => {
+  if (err) {
+    console.log(err);
+    //return next(err);
+  } else {
+    // Si la connexion réussit, poursuivre le code
+    console.log('Connecté à la base de données MySQL');
+  }
+});
+
+// Synchronisez les modèles avec la base de données
+// sequelize
+//   .sync({
+//     //alter: true,
+//   })
+//   .then(() => {
+//     console.log(`Serveur en cours d'exécution sur le port ${port}`);
+//   });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
---
+++ src/sequelize.js
@@ -0,0 +1,15 @@
+import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+export default sequelize;
